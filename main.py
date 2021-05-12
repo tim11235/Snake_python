c0b8fcbda7f374bd1f7daaa025e0ff49b27b70d2
Mbagatuzinde Ikuzwe <ikuz15@gmail.com>
2021-05-12 14:41:15 +0200

    saving high score option

--- main.py
+++ main.py
@@ -36,14 +36,15 @@ while game_is_on:
 
     # Detects collision with the wall.
     if snake.head.xcor() > 390 or snake.head.xcor() < - 390 or snake.head.ycor() > 390 or snake.head.ycor() < -390:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     # Detect collision with tail
     for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
-
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
